6ae9bc115a6ee3ace3727f26ea11cfeac8f5785e
The Floating String <laurencel2001@gmail.com>
2019-08-16 01:47:25 -0400

    Add gunicorn dependency and training graphs

--- src/plot_data.py
+++ src/plot_data.py
@@ -6,6 +6,20 @@ with open('data/training_hist.json', 'r') as json_file:
 
 	print(data)
 
-	plt.plot(data["acc"])
-	plt.plot(data["val_acc"])
+	plt.title("Convolutional Neural Network Training Accuracies")
+	plt.plot(data["acc"], label="training data")
+	plt.plot(data["val_acc"], label="testing data")
+	plt.xlabel("Epochs")
+	plt.ylabel("Accuarcy (%)")
+	plt.legend()
+	plt.savefig("data/accuracy_performance.png")
+
+	plt.close()
+
+	plt.title("Convolutional Neural Network Training Loss")
+	plt.plot(data["acc"], label="training data")
+	plt.plot(data["val_acc"], label="testing data")
+	plt.xlabel("Epochs")
+	plt.ylabel("Loss (mean squared error)")
+	plt.legend()
 	plt.savefig("data/loss_performance.png")
